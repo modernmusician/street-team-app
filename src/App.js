909316e83c1d118a68d4a2ba2f8d7f7f3f2638e4
Scott Landes <scottlandes@gmail.com>
2021-09-13 14:19:08 -0700

    Fan magnet step 2 (#3)
    * updates
    
    * Implement fan magnet step two
    
    * Styles update
    
    * Update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { Routes } from './Routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { FanMagnetProvider } from './context/FanMagnetContext';
 
-
-
-// NOTE: the default AWSAppSyncClient uses an outdated apollo-client dependancy
+// NOTE: the default AWSAppSyncClient uses an outdated apollo-client dependency
 // we're building a custom client instead
 // - see this article https://medium.com/@guillac124/create-your-custom-apollo-client-for-aws-appsync-to-use-hooks-2d5cbce29db5
 
@@ -13,12 +12,13 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { ThemeProvider } from './Components/ThemeProvider';
 import { SecureClient } from './Components/ApolloProvider';
 
-
 function App() {
   return (
     <ApolloProvider client={SecureClient}>
       <ThemeProvider>
-        <Routes />
+        <FanMagnetProvider>
+          <Routes />
+        </FanMagnetProvider>
       </ThemeProvider>
     </ApolloProvider>
   );
---
+++ src/Features/Enduser/Actions/views/FanMagnetStep2.jsx
@@ -0,0 +1,72 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { FanMagnetButton } from '../../../../Components/UI';
+import { FanMagnetHeader } from '../FanMagnetHeader';
+import {
+  FanMagnetContext,
+  SURVEY_SELECTION,
+} from '../../../../context/FanMagnetContext';
+
+const Step2MagnetHeader = styled(FanMagnetHeader)`
+  && {
+    font-size: 60px;
+    margin-bottom: 35px;
+  }
+`;
+
+const MagnetButton = styled(FanMagnetButton)`
+  margin: 0 0 50px;
+  font-size: 51px;
+  font-weight: 400;
+  padding: 30px 47px;
+
+  span {
+    font-size: 55px;
+    margin-right: 43px;
+  }
+`;
+
+export const FanMagnetStep2 = () => {
+  const { setFanMagnetSurvey } = useContext(FanMagnetContext);
+  return (
+    <React.Fragment>
+      <Step2MagnetHeader>Did you enjoy the song?</Step2MagnetHeader>
+      <MagnetButton
+        active
+        activeBgColor="linear-gradient(0deg, rgba(45,132,86,1) 0%, rgba(61,175,114,1) 100%);"
+        activeColor="white"
+        margin="0 0 50px"
+        handleClick={() => {
+          setFanMagnetSurvey(SURVEY_SELECTION.LOVE_IT);
+        }}
+      >
+        <span>👍</span>
+        <div>{SURVEY_SELECTION.LOVE_IT}</div>
+      </MagnetButton>
+      <MagnetButton
+        active
+        activeBgColor="linear-gradient(0deg, rgba(77,77,77,1) 0%, rgba(113,113,113,1) 100%);"
+        activeColor="white"
+        margin="0 0 50px"
+        handleClick={() => {
+          setFanMagnetSurvey(SURVEY_SELECTION.OKAY);
+        }}
+      >
+        <span>👌</span>
+        <div>{SURVEY_SELECTION.OKAY}</div>
+      </MagnetButton>
+      <MagnetButton
+        active
+        activeBgColor="linear-gradient(0deg, rgba(143,37,40,1) 0%, rgba(183,47,51,1) 100%);"
+        activeColor="white"
+        margin="0 0 50px"
+        handleClick={() => {
+          setFanMagnetSurvey(SURVEY_SELECTION.DISLIKE);
+        }}
+      >
+        <span>👌</span>
+        <div>{SURVEY_SELECTION.DISLIKE}</div>
+      </MagnetButton>
+    </React.Fragment>
+  );
+};
