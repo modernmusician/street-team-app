2623bf3b44c3f804ada0efc36b8f02102f0468a3
Scott Landes <scottlandes@gmail.com>
2021-10-17 16:55:29 -0700

    Implement react table + UI

--- src/Features/Admin/Audience/views/AudienceView.jsx
+++ src/Features/Admin/Audience/views/AudienceView.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Icon } from '../../../../Components/UI';
+import { useTable, useSortBy } from 'react-table';
 import { useQuery } from '@apollo/react-hooks';
 import { Container } from 'react-bootstrap';
 import { NavBar } from '../../CreateActions/NavBar';
@@ -9,26 +13,135 @@ import { getAllSubscribersFromArtistUser } from '../queries';
 
 const TableContainer = styled.div`
   background-color: #dddddd;
+  border-radius: 5px;
+  padding: 2rem;
+
+  table {
+    background-color: white;
+    border-radius: 5px;
+    border-spacing: 0;
+    padding: 10px;
+    width: 100%;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    td {
+      margin: 0;
+      padding: 0.5rem 10px;
+    }
+
+    th {
+      padding: 15px 10px;
+      position: relative;
+      text-decoration: ${props => (props.isSorted ? 'underline' : 'none')};
+    }
+  }
+
+  thead {
+    border-bottom: 1px solid #ebebeb;
+    color: #6850ea;
+    padding: 15px 0;
+  }
+
+  tbody {
+    tr:nth-child(odd) {
+      background-color: #f2f2f2;
+    }
+  }
 `;
 
-const endUser = input => {
-  const summary = {};
+const SortIndicator = styled.span`
+  position: absolute;
+  top: 13px;
+  left: 0;
+`;
 
-  summary.email = input?.enduser?.email;
-  summary.firstName = input?.enduser?.firstName;
-  summary.lastName = input?.enduser?.lastName;
-  summary.phone = input?.enduser?.phoneNumber;
-  summary.id = input?.enduserID;
-  summary.totalPoints =
-    input?.enduserPageSubscriptionCompletedActions?.items.reduce(
-      (a, b) => a + b,
-      0
+const TableHeaderText = styled.span`
+  margin-left: 10px;
+`;
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy
     );
 
-  return summary;
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                isSorted={column.isSorted}
+              >
+                <SortIndicator>
+                  <Icon name="FaSort" color="black" size={20} />
+                </SortIndicator>
+                <TableHeaderText>{column.render('Header')}</TableHeaderText>
+                {/* Add a sort direction indicator */}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+Table.propTypes = {
+  columns: PropTypes.func.isRequired,
+  data: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export const AudienceView = () => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Email',
+        accessor: 'email',
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone',
+      },
+      {
+        Header: 'Points',
+        accessor: 'points',
+      },
+    ],
+    []
+  );
+
   const { userId } = useCurrentAuthUser();
   const { data, error, loading } = useQuery(getAllSubscribersFromArtistUser, {
     variables: {
@@ -39,9 +152,26 @@ export const AudienceView = () => {
   if (loading || error || !data) return null;
 
   const endUserData =
-    data.getArtistUser.artist.actionPages.items[0].subscribers.items.map(item =>
-      endUser(item)
+    data.getArtistUser.artist.actionPages.items[0].subscribers.items.map(
+      item => {
+        const firstName = item?.enduser?.firstName || '';
+        const lastName = item?.enduser?.lastName || '';
+
+        return {
+          email: item?.enduser?.email,
+          firstName,
+          lastName,
+          name: `${firstName} ${lastName}`,
+          phone: item?.enduser?.phoneNumber || '',
+          points: item?.enduserPageSubscriptionCompletedActions?.items.reduce(
+            (a, b) => a + b,
+            0
+          ),
+        };
+      }
     );
+
+  console.log(endUserData);
 
   return (
     <React.Fragment>
@@ -49,16 +179,7 @@ export const AudienceView = () => {
       <RootContainer fluid>
         <Container fluid>
           <TableContainer>
-            This is where we will put our dynamic audience data
-            {endUserData &&
-              endUserData.map(user => (
-                <div>
-                  <h1>{user.email}</h1>
-                  <h1>{user.totalPoints}</h1>
-                  <h2>{user.firstName || '[no name]'}</h2>
-                  <h2>{user.phoneNumber}</h2>
-                </div>
-              ))}
+            <Table columns={columns} data={endUserData} />
           </TableContainer>
         </Container>
       </RootContainer>
